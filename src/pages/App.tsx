c15ff9d41bacdc486e3b6676ed345ab81b993a56
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-12 20:14:06 -0300

    Refactor project structure and update main components for movie listing

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <section className="lista">
+      <h1>Filmes</h1>
+      <ul>
+        <li>Harry Potter</li>
+        <li>Jogos Vorazes</li>
+      </ul>
+    </section>
+  );
+}
+
+export default App;
