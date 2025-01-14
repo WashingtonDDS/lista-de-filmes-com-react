299cc033f802a20a706011f78dc4ed438ced4ba5
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-13 22:13:10 -0300

    Implement movie deletion functionality in Lista component

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 import styles from "./lista.module.css";
 
 export const Lista = (): JSX.Element => {
-  const [filmes, setFilmes] = useState<string[]>([
+  const [movies, setMoviess] = useState<string[]>([
     "Harry Potter",
     "Jogos Vorazes",
     "Senhor dos Aneis",
   ]);
+
+  const deletarmovies = (filme: string) => {
+    const newMovies = movies.filter((item) => item !== filme);
+    setMoviess(newMovies);
+  };
   return (
     <ul className={styles["lista-container"]}>
-      {filmes.map((filme) => (
-        <li key={filme} className={styles.lista__item}>
-          {filme}
+      {movies.map((movies) => (
+        <li key={movies} className={styles.lista__item}>
+          {movies}
+          <button onClick={() => deletarmovies(movies)}>Excluir</button>
         </li>
       ))}
     </ul>
