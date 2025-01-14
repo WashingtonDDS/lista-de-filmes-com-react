75a322ade47b432dc6eecd5e86df36bec321419d
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-14 18:17:14 -0300

    Refactor Lista component to accept props for movie items and state management; add movie addition functionality in App component

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import styles from "./lista.module.css";
 
-export const Lista = (): JSX.Element => {
-  const [movies, setMoviess] = useState<string[]>([
-    "Harry Potter",
-    "Jogos Vorazes",
-    "Senhor dos Aneis",
-  ]);
+type TListaProps = {
+  items: string[];
+  setItems: Dispatch<SetStateAction<string[]>>;
+};
 
-  const deletarmovies = (filme: string) => {
-    const newMovies = movies.filter((item) => item !== filme);
-    setMoviess(newMovies);
+export const Lista = ({ items, setItems }: TListaProps): JSX.Element => {
+  const deletarFilmes = (filme: string) => {
+    const novosFilmes = items.filter((item) => item !== filme);
+    setItems(novosFilmes);
   };
   return (
     <ul className={styles["lista-container"]}>
-      {movies.map((movies) => (
-        <li key={movies} className={styles.lista__item}>
-          {movies}
-          <button onClick={() => deletarmovies(movies)}>Excluir</button>
+      {items.map((filme) => (
+        <li key={filme} className={styles.lista__item}>
+          {filme}
+          <button onClick={() => deletarFilmes(filme)}>Excluir</button>
         </li>
       ))}
     </ul>
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { Lista } from "../../components/Lista";
 import styles from "./app.module.css";
 
 function App() {
+  const [filmes, setfilmes] = useState<string[]>([
+    "Harry Potter",
+    "Jogos Vorazes",
+    "Senhor dos Aneis",
+  ]);
+
+  const addFilme = () => {
+    const novoFilmes = [...filmes, "Divergentes"];
+    setfilmes(novoFilmes);
+  };
   return (
     <section className={styles.lista}>
-      <h1 className={styles.title}>Filmes</h1>
-      <Lista />
+      <h1 className={styles.title}>Filmes ({filmes.length})</h1>
+      <Lista items={filmes} setItems={setfilmes} />
+      <button onClick={addFilme}>Adicionar Filme</button>
     </section>
   );
 }
