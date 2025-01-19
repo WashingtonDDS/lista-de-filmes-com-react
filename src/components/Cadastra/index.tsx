6dac93b21dd306a344ed52adcb01770aced7faaa
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-19 20:39:59 -0300

    Add input field and button to Cadastra component form

--- src/components/Cadastra/index.tsx
+++ src/components/Cadastra/index.tsx
@@ -1,4 +1,15 @@
 import styles from "./cadastra.module.css";
 export const Cadastra = () => {
-  return <form className={styles.formulario}></form>;
+  return (
+    <form className={styles.formulario}>
+      <input
+        type="text"
+        className={styles.formulario__nomeFilme}
+        placeholder="Digite o nome do filme"
+      />
+      <button className={`botao ${styles.botao__cadastrar}`}>
+        Adicionar Filme
+      </button>
+    </form>
+  );
 };
